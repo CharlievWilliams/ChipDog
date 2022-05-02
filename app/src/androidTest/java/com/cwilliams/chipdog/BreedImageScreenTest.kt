52a967a577deb1da939df4182e6dc3ab67a263e0
Charlie Williams <charliewilliamsweb@gmail.com>
2022-05-02 14:53:40 +0100

    Fix UI tests

--- app/src/androidTest/java/com/cwilliams/chipdog/BreedImageScreenTest.kt
+++ app/src/androidTest/java/com/cwilliams/chipdog/BreedImageScreenTest.kt
@@ -1,10 +1,11 @@
 package com.cwilliams.chipdog
 
+import androidx.activity.viewModels
 import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.ui.test.*
-import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import com.cwilliams.chipdog.ui.theme.ChipDogTheme
 import com.cwilliams.chipdog.view.screen.BreedImageScreen
 import com.cwilliams.chipdog.viewModel.BreedImageViewModel
@@ -13,7 +14,6 @@ import dagger.hilt.android.testing.HiltAndroidTest
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
-import javax.inject.Inject
 
 @ExperimentalFoundationApi
 @ExperimentalMaterial3Api
@@ -22,37 +22,51 @@ import javax.inject.Inject
 class BreedImageScreenTest {
 
     @get:Rule(order = 0)
-    val composeTestRule = createComposeRule()
-
-    @get:Rule(order = 1)
     val hiltRule = HiltAndroidRule(this)
 
-    @Inject
-    lateinit var viewModel: BreedImageViewModel
+    @get:Rule(order = 1)
+    val composeTestRule = createAndroidComposeRule<MainActivity>()
 
     @Before
     fun init() {
         hiltRule.inject()
+
     }
 
     @Test
-    fun given_when_then() {
-        // Given
+    fun givenValidBreed_whenScreenLoad_thenDisplayBreed() {
+        // Given & When
         composeTestRule.setContent {
             ChipDogTheme {
-                BreedImageScreen(viewModel = viewModel, name = NAME)
+                BreedImageScreen(
+                    viewModel = composeTestRule.activity.viewModels<BreedImageViewModel>().value,
+                    name = VALID_NAME
+                )
             }
         }
-        composeTestRule.waitForIdle()
-
-        // When
-        composeTestRule.onNodeWithContentDescription("Button").performTouchInput { swipeUp() }
 
         // Then
-        composeTestRule.onNodeWithText("Scroll to top").assertIsDisplayed()
+        composeTestRule.onNodeWithText(VALID_NAME, ignoreCase = true).assertIsDisplayed()
+    }
+
+    @Test
+    fun givenInvalidBreed_whenScreenLoad_thenDisplayError() {
+        // Given & When
+        composeTestRule.setContent {
+            ChipDogTheme {
+                BreedImageScreen(
+                    viewModel = composeTestRule.activity.viewModels<BreedImageViewModel>().value,
+                    name = INVALID_NAME
+                )
+            }
+        }
+
+        // Then
+        composeTestRule.onNodeWithText("An error has occurred. Try refreshing.").assertIsDisplayed()
     }
 
     companion object {
-        const val NAME = "Basenji"
+        const val VALID_NAME = "basenji"
+        const val INVALID_NAME = "unknown"
     }
 }
--- app/src/androidTest/java/com/cwilliams/chipdog/BreedListScreenTest.kt
+++ app/src/androidTest/java/com/cwilliams/chipdog/BreedListScreenTest.kt
@@ -1,10 +1,11 @@
 package com.cwilliams.chipdog
 
+import androidx.activity.viewModels
 import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.ui.test.*
-import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import com.cwilliams.chipdog.ui.theme.ChipDogTheme
 import com.cwilliams.chipdog.view.screen.BreedListScreen
 import com.cwilliams.chipdog.viewModel.BreedListViewModel
@@ -13,7 +14,6 @@ import dagger.hilt.android.testing.HiltAndroidTest
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
-import javax.inject.Inject
 
 @ExperimentalFoundationApi
 @ExperimentalMaterial3Api
@@ -22,13 +22,10 @@ import javax.inject.Inject
 class BreedListScreenTest {
 
     @get:Rule(order = 0)
-    val composeTestRule = createComposeRule()
-
-    @get:Rule(order = 1)
     val hiltRule = HiltAndroidRule(this)
 
-    @Inject
-    lateinit var viewModel: BreedListViewModel
+    @get:Rule(order = 1)
+    val composeTestRule = createAndroidComposeRule<MainActivity>()
 
     @Before
     fun init() {
@@ -36,17 +33,35 @@ class BreedListScreenTest {
     }
 
     @Test
-    fun given_when_then() {
-        // Given
+    fun givenListScreen_whenScreenLoad_thenDisplayBreedList() {
+        // Given & When
         composeTestRule.setContent {
             ChipDogTheme {
-                BreedListScreen(viewModel = viewModel, navigateToNextScreen = {})
+                BreedListScreen(
+                    viewModel = composeTestRule.activity.viewModels<BreedListViewModel>().value,
+                    navigateToNextScreen = {}
+                )
             }
         }
-        composeTestRule.waitForIdle()
+
+        // Then
+        composeTestRule.onNodeWithText("Affenpinscher").assertIsDisplayed()
+    }
+
+    @Test
+    fun givenListScreen_whenSwipeUp_thenDisplayScrollToTopFab() {
+        // Given & When
+        composeTestRule.setContent {
+            ChipDogTheme {
+                BreedListScreen(
+                    viewModel = composeTestRule.activity.viewModels<BreedListViewModel>().value,
+                    navigateToNextScreen = {}
+                )
+            }
+        }
 
         // When
-        composeTestRule.onNodeWithContentDescription("Button").performTouchInput { swipeUp() }
+        composeTestRule.onNodeWithText("Affenpinscher").performTouchInput { swipeUp() }
 
         // Then
         composeTestRule.onNodeWithText("Scroll to top").assertIsDisplayed()
